d439e4c5f293fbde3e90f8627ac07bacd7a01c64
Arek Juszczyk <arek.juszczyk@ed.ac.uk>
2026-07-16 16:43:05 +0200

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -53,7 +53,7 @@ module.exports = function (grunt) {
 					'!README.md',
 					'!HOWTO.md',
 					'!SECURITY.md',
-					'!phpcs.xml', '!phpcs.xml.dist', '!phpstan.neon.dist', '!grumphp.yml.dist', // CodeSniffer Configuration.
+					'!phpcs.xml', '!phpcs.xml.dist', '!phpstan.neon.dist', '!grumphp.yml.dist', '!grumphp.yml', // CodeSniffer Configuration.
 					'!docker-compose.override.yml', // Local Docker Development configuration.
 					'!codecov.yml', // Code coverage configuration.
 					'!tools/**', // Local Development/Build tools configuration.
